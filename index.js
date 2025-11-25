9ac6016dcb7bd6e5481be397bee1765374d911bf
Mirza Shoaib Ali <mirzashoaibali607@gmail.com>
2025-11-25 20:37:06 +0530

    commit changes

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use("/api/v1/course", courseRouter);
 
 async function main() {
     await mongoose.connect(process.env.MONGO_URL);
-    app.listen(3000);
+    app.listen(process.env.PORT || 3000);
     console.log("listening on port: 3000");
 }
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const adminRouter = Router();
-const { adminModel } = require("../db");
+const { courseModel, adminModel } = require("../db");
 const jwt = require("jsonwebtoken");
 
 const { JWT_ADMIN_PASSWORD } = require("../config");
