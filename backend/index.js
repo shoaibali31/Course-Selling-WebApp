89e73e6a3bbc8bdc5bce84e484486f3f0624b735
Mirza Shoaib Ali <mirzashoaibali607@gmail.com>
2025-11-29 15:03:12 +0530

    commit changes

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,14 @@ require('dotenv').config()
 console.log(process.env.MONGO_URL);
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
 const { adminRouter } = require("./routes/admin");
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/user", userRouter);
